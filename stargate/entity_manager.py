f2fb683faf6e6734552ccd166c66b55a9859f962
sohaibfarooqi <sohaibfarooqi@hotmail.com>
2016-12-11 16:28:08 +0500

    working with query filters

--- stargate/entity_manager.py
+++ stargate/entity_manager.py
@@ -18,112 +18,81 @@ class EntityManager():
 				if query_string is not None:
 					
 					query_string_dict = dict(urlparse.parse_qsl(query_string, encoding = 'utf-8'))
-					query_filters = QueryFilters()
-					
-					filters,operator = query_filters.group_logical_operators(query_string_dict['filters'])
+					group_filters, rest_operator = QueryFilters.group_logical_operators(query_string_dict['filters'],model)
+
 					query = model.query
-					filtr = list()
-
-					for key in filters:
-
-						logical_op = query_filters.get_logical_operator(key)
-						
-						split_filter = key.strip()
-
-						if logical_op is None:
-							temp = dict()
-							temp = {'expr': query_filters.get_filter_expression(split_filter, op, model), 'op': None}
-							op = query_filters.get_column_operator(split_filter)
-							filtr.append(temp)
-						
-						else:
-
-							base_filters = split_filter.split(logical_op)
-							temp1 = list()
-							for filter_exp in base_filters:
-
-								filter_exp = filter_exp.strip()
-								op = query_filters.get_column_operator(filter_exp)
-								temp1.append(query_filters.get_filter_expression(filter_exp, op, model))
-							
-							filtr.append({'expr': temp1, 'op': logical_op})	
-					query = query.filter((query_filters.get_sql_operator(operator)(*[query_filters.get_sql_operator(filter['op'])(*filter['expr']) for filter in filtr ])))		
+					
+					for key in group_filters:
+						query = query.filter((QueryFilters.get_sql_operator(key['op']))(*[(filters) for filters in key['expr']]))
+					
+					query = query.filter((QueryFilters.get_sql_operator(rest_operator['op'])(*[(filters) for filters in rest_operator['expr']])))	
 					print(query)
 			else:
-				query = model.query.get(pk_id)
+				#Default collection critera
 				return None
 
 		else:
+			query = model.query.get(pk_id)
 			return None
 
 class QueryFilters():
 	
-	REGEX_LOGICAL_GROUPS = r'(\)+\s+\b(and|or)\b\s+\(?)'
 	REGEX_FILTER_GROUPS = r'\((.*?)\)'
-	REGEX_LOGICAL_OPERATORS = r'\s+\b(and|or)\b\s+'
+	REGEX_LOGICAL_OPERATORS = r'\s+(and|or)\s+'
 	REGEX_COLUMN_OPERATORS = r'\s+(\w+)\s+'
-	GROUP_DELIMITER = ')'
-
-	def __init__(self):
-		self.filters = list()
-		self.op = None
-		self.span_array = list()
-
-	def group_logical_operators(self, query_string_dict):
+	REGEX_EXPRESSION_PARSER = r'(((?:(and|or)\s+)*\((%s)\))(?:\s+(and|or))*)'
+	
+	def group_logical_operators(query_string_dict, model):
 		
-		r = re.compile(self.REGEX_FILTER_GROUPS)
+		r = re.compile(QueryFilters.REGEX_FILTER_GROUPS)
 		iterator = r.finditer(query_string_dict)
 		
-		
-		group_boundries = list()
-		match_filters = ''
+		filters, group_boundries, group_filters, operators = list(), list(), list(), set()
 		filters, group_boundries = zip(*[(match.group(1), match.span()) for match in iterator])
+		
 		remaining_str = query_string_dict
-		for key in filters:
-			# remaining_str = re.split(r'(?:(and|or)\s+)*(\(%s\))(?:\s+(and|or))*' % key, query_string_dict)
-			remaining_str = re.split(r'and|or\s+%s\s+and|or' % key, query_string_dict)
-			print(remaining_str)
+		remaining_filters = ''
+		
+		for count, key in enumerate(filters):
+			
+			filter_expr,filter_expr_op = QueryFilters.parse_simple_expression(key, model)
+			group_filters.append({'expr': filter_expr, 'op': filter_expr_op})
+			
+			match_string = list()
+
+			match_string = re.search(r'(((?:(and|or)\s+)*\((%s)\))(?:\s+(and|or))*)' % key, query_string_dict).groups()
+			
+			expr_with_both_operator = match_string[0]
+			expr_with_pre_operator = match_string[1]
+			pre_operator = match_string[2]
+			post_operator = match_string[4]
+			
+			if group_boundries[count][1] <= len(query_string_dict) and group_boundries[count][0] != 0:
+				string_to_match = expr_with_pre_operator
+			else:
+				string_to_match = expr_with_both_operator
+			
+			if pre_operator is None:
+				print("Group At Start")
+			else:
+				operators.add(pre_operator)
+			
+			if post_operator is None:
+				print("Group At End")
+			else:
+				operators.add(post_operator)
+			
+			remaining_str =  remaining_str.replace(string_to_match,'')
+
+		remaining_str = re.sub(r'\s+', ' ', remaining_str)
+		remaining_str = remaining_str.strip()
+		remaining_filters,op = QueryFilters.parse_simple_expression(remaining_str,model)
+		remaining_filters_dict = {'expr': remaining_filters, 'op': op}
+		
+		return group_filters, remaining_filters_dict
 		
 
-			
-		for count, key in enumerate(group_boundries):
-			remaining_str = remaining_str[0:key[0]-length] + remaining_str[key[1]-curr_length:len(remaining_str)-1]
-			length = group_boundries[count-1][1] - group_boundries[count-1][0] 
-			curr_length = group_boundries[count][1] - group_boundries[count][0] 
-			# print(remaining_str) 
-			# print(length)
-			# print(curr_length)
-			# print(key[0]-length)
-			# print(key[1]-length-curr_length, len(remaining_str))
-		remaining_str = remaining_str.lstrip().rstrip()
-		# print(remaining_str) 
-		# for match in iterator:
-
-		# 	self.filters.append(match.group(1))
-		# 	temp = match.span()
-
-		# 	if not span_element:
-		# 		span_element.insert(0,temp[1])
-			
-		# 	else:
-		# 		span_element.insert(1, temp[0])
-
-		# 	if len(span_element) == 2:
-		# 		self.span_array.append(span_element)
-		# 		span_element = list()
-
-		for key in self.span_array:
-			op = query_string_dict[key[0]:key[1]].replace(' ','')
-			
-			if self.op is None:
-				self.op = op
-			
-			else:
-				if op != self.op:
-					print("Multiple different logical operators found at same level")
-		return self.filters, self.op
-
-	def get_sql_operator(self, op):
+	def get_sql_operator(op):
 		
 		if op == 'and':
 			return and_
@@ -134,41 +103,44 @@ class QueryFilters():
 		else:
 			return None
 
+	def parse_simple_expression(expr,model):
+
+		operator = set()
+		operator = set([op for op in QueryFilters.get_logical_operator(expr)])
+		bool_operator = next(iter(operator))
+		base_filters = expr.split(bool_operator)
+		filters = list()
 		
-	def internal_logical_groups(self, expr):
-		r = re.compile(self.REGEX_LOGICAL_GROUPS, flags = re.I | re.X)
+		for filter_exp in base_filters:
+			filter_exp = filter_exp.strip()
+			op = QueryFilters.get_column_operator(filter_exp)
+			filters.append(QueryFilters.get_filter_expression(filter_exp, op, model))
+		return filters,bool_operator
 
-		for key in expr:
-			iterator = r.finditer(key)
-			
-			for match in iterator:
-				print(match)
-				node = FilterNode()
-				node.create_node(	range = match.span(), 
-							operator = match.group(2), 
-							matched_operator = match.group(),
-							level = match.group().count(self.GROUP_DELIMITER),
-							query_string = key
-						  )
-				self.filters.append(node)
+	def get_logical_operator(str):
 		
-				
+		op_list = re.findall(QueryFilters.REGEX_LOGICAL_OPERATORS, str, flags = re.I)
 
-
-	def get_logical_operator(self, str):
-		r = re.search(self.REGEX_LOGICAL_OPERATORS, str, flags = re.I)
-		if r is not None:
-			return r.group().strip()
+		if op_list:
+			return op_list
+		
 		else:
 			return None
-
-	def get_column_operator(self, exp):
+			
+	def get_column_operator(exp):
 		exp = exp.strip()
-		op = re.search(self.REGEX_COLUMN_OPERATORS, exp, flags = re.I).group().strip()
-		return op
+		op = re.search(QueryFilters.REGEX_COLUMN_OPERATORS, exp, flags = re.I)
+
+		if op is None:
+			print("No Opeator Found")
+			return None
+		
+		else:
+			return op.group().strip()
 							
-	def get_filter_expression(self, expression, op, model):
-		column, value = expression.split(op)
+	def get_filter_expression(expression, op, model):
+
+		column, value = re.split(r'\s%s\s' % op, expression)
 		#TODO: check if column exist and conform value received in request
 		column = column.replace('(','')
 		column = column.replace(')','')
@@ -177,55 +149,9 @@ class QueryFilters():
 		value = value.replace('(','')
 		value = value.replace(')','')
 		value = value.replace(' ','')
-
 		field = getattr(model, column, None)
 		attr = list(filter(
 						lambda e: hasattr(field, e % op), 
 						['%s','%s_','__%s__']
 					  ))[0] % op
 		return getattr(field, attr)(value)
-
-	def create_query(self, query, op, filters):
-
-		if op == 'and':
-			return query.filter(and_(*filters))
-
-		elif op == 'or':
-			return query.filter(or_(*filters))
-
-		else:
-			print("No Operator Found %s" %op)
-
-	def append_query(self, query, op, filters):
-
-		if op == 'and':
-			return query.filter(and_(filters))
-
-		elif op == 'or':
-			return query.filter(or_(filters))
-
-		else:
-			print("No Operator Found %s" %op)
-
-
-class FilterNode():
-	
-	def __init__(self, **kwargs):
-		self.range = list()
-		self.operator = None
-		self.matched_operator = None
-		self.level =  None
-		self.split_result = None
-
-	def create_node(self, **kwargs):
-		self.range = kwargs['range']
-		self.operator = kwargs['operator']
-		self.matched_operator = kwargs['matched_operator']
-		self.level = kwargs['level']
-		self.split_result = self._split_filter(self.range, kwargs['query_string'])
-
-	def _split_filter(self, range, query_string):
-		init_range = range[0] + 1
-		end_range = range[1] - 1
-		length = len(query_string)
-		return query_string[0:init_range], query_string[end_range:length]
