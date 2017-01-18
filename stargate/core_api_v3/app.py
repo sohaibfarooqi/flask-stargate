c4dc332c7a1f8abfad0a55fc8a3664398291f6ad
Sohaib Farooqi <muhammad.sohaib@zameen.com>
2017-01-18 17:55:11 +0500

    new structure

--- stargate/core_api_v3/app.py
+++ stargate/core_api_v3/app.py
@@ -16,32 +16,41 @@ class Application(Flask):
 	__VERSION__ = '0.1.0'
 
 	#Apopted from => http://flask.pocoo.org/docs/0.12/patterns/subclassing/
-    request_class = RequestCls
-    response_class = ResponseCls
+	request_class = RequestCls
+	response_class = ResponseCls
 
 
-   	def __init__(self, app_url_prefix = None, *args, **kwargs):
-    	"""Application constructor registering application setting and initilizing Flask app. 
-    	"""
+	def __init__(self, app_url_prefix = None, *args, **kwargs):
+		"""Application constructor registering application setting and initilizing Flask app. 
+		"""
 		super(Application, self).__init__(*args, **kwargs)
-        self.api_settings = ApplicationSettings(self.config)
-        #default url prefix for app context.
-        if app_url_prefix is not None:
-        	bp = Blueprint(str(uuid1()), __name__, url_prefix = app_url_prefix)
-        	self.register_blueprint(bp)
+		self.api_settings = ApplicationSettings(self.config)
+		#default url prefix for app context.
+		if app_url_prefix is not None:
+			bp = Blueprint(str(uuid1()), __name__, url_prefix = app_url_prefix)
+			self.register_blueprint(bp)
 
-   	def preprocess_request(self):
-   		"""Method overriden from Flask to perform any application specific preprocessing. 
-   		"""
-   	def register_resource_as_api(self, *args, **kwargs):
-   		"""Register callable endpoints for a particular resource.
-   		"""	
-   	def make_response(self, rv):
-   		"""Method overriden from Flask to perform any application specific postprocessing. 
-   		"""
-  	def handle_user_exception(self, e):
-  		"""Method overriden from Flask to format error in Application specific format. 
-   		"""
-  	def handle_api_exception(self, exc):
-  		"""Method to handle Api Exception. 
-   		"""
+	def preprocess_request(self):
+		"""Method overriden from Flask to perform any application specific preprocessing. 
+		"""
+		pass
+
+	def register_resource_as_api(self, *args, **kwargs):
+		"""Register callable endpoints for a particular resource.
+		"""	
+		pass
+
+	def make_response(self, rv):
+		"""Method overriden from Flask to perform any application specific postprocessing. 
+		"""
+		pass
+
+	def handle_user_exception(self, e):
+		"""Method overriden from Flask to format error in Application specific format. 
+		"""
+		pass
+
+	def handle_api_exception(self, exc):
+		"""Method to handle Api Exception. 
+		"""
+		pass
