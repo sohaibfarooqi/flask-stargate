83fc2a774f74f4bd444b9f9e2365e910bbb4cb82
sohaibfarooqi <sohaibfarooqi@hotmail.com>
2017-02-12 15:07:05 +0500

    travisCI setup

--- wsgi.py
+++ wsgi.py
@@ -3,11 +3,13 @@ from flask import g, Flask
 from flask_sqlalchemy import SQLAlchemy
 from .models import User, Location, City
 from .stargate.core_api import ResourceManager
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 db = SQLAlchemy()
 db.init_app(app)
+Migrate(app, db)
 manager = ResourceManager(app, flask_sqlalchemy_db = db)
 
 manager.register_resource(User, methods = ['GET', 'POST', 'DELETE'])
